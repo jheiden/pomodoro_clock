cf50145009cc91dcab1be2523c522e4ce91253a8
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2019-01-22 22:14:18 +0100

    implemented layout

--- tween.js
+++ tween.js
@@ -1,8 +1,12 @@
 
-document.getElementById('btn').addEventListener('click', () => {
+document.getElementById('js-play').addEventListener('click', () => {
 	const boxHeight = 60;
 	const animationHeight = containerHeight() - boxHeight;
-	TweenMax.staggerTo('.box', 2.5,  {y: animationHeight, ease:Bounce.easeOut}, 0.5);
+	TweenMax.staggerTo('.box', 60,  {y: animationHeight, ease:Expo.easeOut}, 0.5);
+});
+
+document.getElementById('js-stop').addEventListener('click', () => {
+	TweenMax.staggerTo('.box', 2,  {y: 0, ease:Expo.easeOut}, 0.1);
 });
 
 const containerHeight = () => {

16540d83cf489c15a876db16fa710dc773c2c43c
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2019-01-23 15:47:34 +0100

    prototype for tween

--- tween.js
+++ tween.js
@@ -1,14 +1,104 @@
 
 document.getElementById('js-play').addEventListener('click', () => {
-	const boxHeight = 60;
-	const animationHeight = containerHeight() - boxHeight;
-	TweenMax.staggerTo('.box', 60,  {y: animationHeight, ease:Expo.easeOut}, 0.5);
+	interval = setInterval(countDown, 1000);
+	animate();
 });
 
 document.getElementById('js-stop').addEventListener('click', () => {
-	TweenMax.staggerTo('.box', 2,  {y: 0, ease:Expo.easeOut}, 0.1);
+	clearInterval(interval);
+	TweenMax.staggerTo('.animation-wrapper', .5,  {y: -2000, ease:Expo.eastOut}, 0.1);
+
+	setTimeout(() => {
+		destroyAnimations();
+		resetWrapper();
+	}, 500);	
 });
 
-const containerHeight = () => {
-	return document.querySelector('.animation-area').clientHeight;
+const containerHeight = document.querySelector('.animation-wrapper').clientHeight;
+
+
+let boxIterations = 1;
+let timer = 0;
+
+const generateRow = (className, boxHeight, boxWidth, boxes) => {
+	const wrapper = document.querySelector('.animation-wrapper');
+	const area = document.createElement('div');
+	area.classList.add('animation-area');
+	wrapper.append(area);
+
+	for (let index = 0; index < boxes; index++) {
+		const div = document.createElement('div');
+		div.classList.add('box');
+		div.style.height = boxHeight + 'px';
+		div.style.width = boxWidth + 'px';
+		div.classList.add(className);
+		area.append(div);
+	}
 }
+
+const animate = () => {
+	const className = 'box' + boxIterations;
+
+
+	const boxHeight = containerHeight / (25 * 2);
+
+	const width =	window.innerWidth;
+
+	const boxWidth = (width / 2) / (25 * 2);
+
+	const boxes = Math.floor(width / boxWidth);
+
+	generateRow(className, boxHeight, boxWidth, boxes);
+
+	const negativeMargin = 60;
+	const animationHeight = containerHeight - (boxHeight * boxIterations) + negativeMargin;
+	const currentClass = '.' + className;
+
+	const elements = Array.from(document.querySelectorAll(currentClass));
+	elements.sort(function(){return 0.5-Math.random()});
+
+	TweenMax.staggerTo(elements, 60,  {y: animationHeight, ease: getRandomAnimation()}, 0.5);
+	boxIterations++;
+}
+
+const destroyAnimations = () => {
+	const wrapper = document.querySelector('.animation-wrapper');
+	wrapper.innerHTML = null;
+}
+
+const resetWrapper = () => {
+	const wrapper = document.querySelector('.animation-wrapper');
+	wrapper.style.transform = 'matrix(1, 0, 0, 1, 0, 0)';
+	boxIterations = 1;
+}
+
+const getRandomAnimation = () => {
+	 const num = Math.floor(Math.random() * 6);
+
+	 console.log('num', num);
+
+	 switch(num) {
+		 case 1: return 'Expo.easeOut';
+		 case 2: return 'Bounce.easeOut';
+		 case 3: return 'Power0.easeOut';
+		 case 4: return 'Power1.easeOut';
+		 case 5: return 'Power2.easeOut';
+	 }
+}
+
+function countDown () {
+
+	timer++;
+
+		// animate every 10th seconds
+	if (timer % 30 === 0) {
+		animate();
+	}
+
+	if (timer === 60 * 25) {
+		clearInterval(interval);
+	}
+
+
+}
+

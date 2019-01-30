55fb5d4983c65bb837e0e65453e87114211fd83e
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2019-01-30 16:30:21 +0100

    linted

--- timer.js
+++ timer.js
@@ -1,80 +1,78 @@
 // TODO => start break 5 min after first work session 25 min and then back to work again.
 class Timer {
-  // init: function .. ?
-  constructor() {
-    this.workLength = 5;
-    this.breakLength = 3;
-    // odd number = work , even number = break
-    this.state = 1;
-    // UI
-    this.display = document.querySelector(".timer");
-    this.startBtn = document.querySelector("#js-play");
-    this.stopBtn = document.querySelector("#js-stop");
+	// init: function .. ?
+	constructor() {
+		this.workLength = 5;
+		this.breakLength = 3;
+		// odd number = work , even number = break
+		this.state = 1;
+		// UI
+		this.display = document.querySelector('.timer');
+		this.startBtn = document.querySelector('#js-play');
+		this.stopBtn = document.querySelector('#js-stop');
 
-    // TODO => disable button after clicked once.
+		// TODO => disable button after clicked once.
 
-    this.startBtn.addEventListener("click", e => {
-      this.startCount();
-    });
+		this.startBtn.addEventListener('click', () => {
+			this.startCount();
+		});
 
-    this.stopBtn.addEventListener("click", e => {
-      this.stopAndReset();
-    });
-  }
+		this.stopBtn.addEventListener('click', () => {
+			this.stopAndReset();
+		});
+	}
 
-  displayTime(timeToTick) {
-    let convertedMinutes = Math.floor(timeToTick / 60);
-    let convertedSeconds = timeToTick % 60;
-    const prefix = convertedSeconds > 9 ? "" : 0;
-    this.display.textContent = `${convertedMinutes}:${prefix}${convertedSeconds}`;
-  }
+	displayTime(timeToTick) {
+		const convertedMinutes = Math.floor(timeToTick / 60);
+		const convertedSeconds = timeToTick % 60;
+		const prefix = convertedSeconds > 9 ? '' : 0;
+		this.display.textContent = `${convertedMinutes}:${prefix}${convertedSeconds}`;
+	}
 
-  // Must use arrow function cause of scope of <this> when used for the setInterval..?
-  startCount() {
-    this.interval = setInterval(() => {
-      this.countDown();
-    }, 1000);
-  }
+	// Must use arrow function cause of scope of <this> when used for the setInterval..?
+	startCount() {
+		this.interval = setInterval(() => {
+			this.countDown();
+		}, 1000);
+	}
 
-  countDown() {
-    if (this.hasEnded()) {
-      this.endTime();
-      this.incrementState();
-      this.resetTimers();
-      this.startCount();
-    } else {
-      this.checkState()
-  
-    }
-  };
+	countDown() {
+		if (this.hasEnded()) {
+			this.endTime();
+			this.incrementState();
+			this.resetTimers();
+			this.startCount();
+		} else {
+			this.checkState();
+		}
+	}
 
-  checkState() {
-    if (this.state % 2 !== 0) {
-      this.workLength --;
-      this.displayTime(this.workLength);
-    } else {
-      this.breakLength --;
-      this.displayTime(this.breakLength);
-    }
-  };
+	checkState() {
+		if (this.state % 2 !== 0) {
+			this.workLength -= 1;
+			this.displayTime(this.workLength);
+		} else {
+			this.breakLength -= 1;
+			this.displayTime(this.breakLength);
+		}
+	}
 
-  hasEnded() {
-    return !this.workLength;
-  }
+	hasEnded() {
+		return !this.workLength;
+	}
 
-  endTime() {
-    clearInterval(this.interval);
-  }
+	endTime() {
+		clearInterval(this.interval);
+	}
 
-  incrementState() {
-    this.state ++;
-  }
+	incrementState() {
+		this.state += 1;
+	}
 
-  resetTimers() {
-    this.workLength = 5;
-    this.breakLength = 3;
-  }
+	resetTimers() {
+		this.workLength = 5;
+		this.breakLength = 3;
+	}
+}
 
-};
-
-new Timer();
+const timer = new Timer();
